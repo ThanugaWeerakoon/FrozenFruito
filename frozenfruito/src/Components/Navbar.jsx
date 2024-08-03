3802ca36d105df6cb174da6b33544aaa3634d030
ThanugaWeerakoon <136422410+ThanugaWeerakoon@users.noreply.github.com>
2024-08-03 16:05:37 +0530

    nav

--- frozenfruito/src/Components/Navbar.jsx
+++ frozenfruito/src/Components/Navbar.jsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar--container">
-        <Link to="/" className="navbar--link">Home</Link>
-        <Link to="/contact" className="navbar--link">Contact Us</Link>
+        <Link to="/" className="navbar--link">Home</Link>        
         <Link to="/products" className="navbar--link">Products</Link>
+        <Link to="/contact" className="navbar--link">Contact Us</Link>
       </div>
     </nav>
   );
