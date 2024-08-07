ba499957cf9fa10c0e053190cec2b61749c84ce4
DilushaFdo <130301311+DilushaFdo@users.noreply.github.com>
2024-08-07 15:38:37 +0530

    Added image enlargement

--- frozenfruito/src/Components/ImageSlider.jsx
+++ frozenfruito/src/Components/ImageSlider.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 
 export const ImageSlider = ({ slides }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalImage, setModalImage] = useState('');
 
     const slideStyles = {
         backgroundImage: `url(${slides[currentIndex].url})`,
@@ -24,6 +26,16 @@ export const ImageSlider = ({ slides }) => {
         setCurrentIndex(slideIndex);
     }
 
+    const openModal = (image) => {
+        setModalImage(image);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setModalImage('');
+    };
+
     return (
         <div>
             <div className='slider-container'>
@@ -41,6 +53,30 @@ export const ImageSlider = ({ slides }) => {
             <div className="title">{slides[currentIndex].title}</div>     
             <div className="description">{slides[currentIndex].description}</div>     
             <div className="details">{slides[currentIndex].details}</div>     
+            <div className="image1">
+            {slides[currentIndex].image1 && (
+                    <img
+                        src={slides[currentIndex].image1}
+                        alt="First slide"
+                        className="image1"
+                        onClick={() => openModal(slides[currentIndex].image1)}
+                    />
+                )}
+            {slides[currentIndex].image2 && (
+                    <img
+                        src={slides[currentIndex].image2}
+                        alt="Second slide"
+                        className="image2"
+                        onClick={() => openModal(slides[currentIndex].image2)}
+                    />
+                )}    
+            </div>
+             
+             {isModalOpen && (
+                <div className="modal" onClick={closeModal}>
+                    <img src={modalImage} alt="Enlarged view" className="modal-image" />
+                </div>
+            )}
         
         </div>
     );
