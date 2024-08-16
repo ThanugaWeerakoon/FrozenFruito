3612a67e1c2ec702a457a2d1c9671a68b8f73c2c
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-08-16 15:56:06 +0530

    Update last

--- frozenfruito/src/Components/Navbar.jsx
+++ frozenfruito/src/Components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from "react";
 import { Link } from "react-router-dom";
- 
 
 export default function Navbar() {
   return (
     <nav className="navbar ">
       <div className="navbar--container">
-        <Link to="/" className="navbar--link">Home</Link>        
-        <Link to="/products" className="navbar--link">Products</Link>
-        <Link to="/contact" className="navbar--link">Contact Us</Link>
+        <Link to="/" className="navbar--link">
+          Home
+        </Link>
+        <Link to="/products" className="navbar--link">
+          Products
+        </Link>
+        <Link to="/contact" className="navbar--link">
+          Contact Us
+        </Link>
       </div>
     </nav>
   );
